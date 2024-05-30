96f41ad416ea8d3f28609d456bfcee9e6838c281
Rudraksh121a <141481238+Rudraksh121a@users.noreply.github.com>
2024-05-30 18:20:25 +0000

    datatypes

---
+++ 01_basics/DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // We  are using nodejs, not browser
+
+let name="Rudra"
+let age=18
+let check=true
+
+/*
+Data Types
+int 
+bigint 
+string => ""
+boolean => true or false
+null => standalone value
+undefined
+symbol => unique
+
+
+object
+
+*/
+console.log(typeof null);
+console.log(typeof undefined);
